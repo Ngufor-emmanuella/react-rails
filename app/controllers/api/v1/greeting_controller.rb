1e7bfeb17b203150e2005c34ffb063b78c874d6c
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-03-29 11:11:57 +0100

    Add controler

--- app/controllers/api/v1/greeting_controller.rb
+++ app/controllers/api/v1/greeting_controller.rb
@@ -1,4 +1,4 @@
-class  Api::V1::GreetingController < ApplicationController
+class Api::V1::GreetingController < ApplicationController
   def index
     @greeting = Greeting.all.sample
     render json: @greeting, status: 200
